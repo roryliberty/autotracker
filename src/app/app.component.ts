6466dccdd7573db6fe563e696000f175ce5e278a
Rory Liberty <roryliberty@gmail.com>
2022-01-26 15:41:20 -0600

    Set up Reactive Form and it now seems to be working; Added Clear button to clear the loadedAutos array;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { AutoModel } from "./auto.model";
+import { FormBuilder, FormGroup, NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,31 @@ import { AutoModel } from "./auto.model";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  autoForm = this.fb.group({
+    year: [''],
+    make: [''],
+    model: ['']
+  })
   loadedAutos: AutoModel[] = [];
 
-  onSubmit(autoData: AutoModel) {
+  constructor(private fb: FormBuilder) {
+  }
+
+  onSubmit(auto: AutoModel) {
     // Save form data to loadedAutos array
-    this.loadedAutos.push(autoData)
+    this.loadedAutos.push(auto);
+    // Clear form fields
+    this.autoForm.reset();
+    // log to console
+    console.log(this.loadedAutos);
+  }
+
+  onClear() {
+    // Clear form fields
+    this.autoForm.reset();
+    // Clear loadedAutos array
+    this.loadedAutos.splice(0);
+    // log to console
+    console.log(this.loadedAutos);
   }
 }
